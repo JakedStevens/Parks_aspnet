de1c13fb212ce82cb10de5d2d5f0a6da00d8774a
Jake Stevens <jake.stevens@improving.com>
2021-06-28 12:33:57 -0500

    refactored caching

--- Parks/Models/CacheService.cs
+++ Parks/Models/CacheService.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Parks.Models
 {
 	public class CacheService
 	{
+		private static readonly HttpClient _httpClient = new();
 		private readonly IMemoryCache _cache;
 
 		public CacheService(IMemoryCache memoryCache)
@@ -16,23 +19,27 @@ namespace Parks.Models
 			_cache = memoryCache;
 		}
 
-		public bool CacheExists()
+		public async Task<List<Park>> GetSetParksCache()
 		{
-			return _cache.TryGetValue("parkList", out List<Park> _);
-		}
-
-		public List<Park> GetCachedData()
-		{
-			return _cache.Get<List<Park>>("parkList");
-		}
-
-		public void SetCachedData(List<Park> parksList)
-		{
-			var cacheEntryOptions = new MemoryCacheEntryOptions()
+			if (!_cache.TryGetValue("parkList", out List<Park> parkList))
 			{
-				AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(5)
-			};
-			_cache.Set("parkList", parksList, cacheEntryOptions);
+				parkList = await BuildParksList();
+
+				var cacheEntryOptions = new MemoryCacheEntryOptions()
+				{
+					AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(5)
+				};
+
+				_cache.Set("parkList", parkList, cacheEntryOptions);
+			}
+			return parkList;
+		}
+
+		public async Task<List<Park>> BuildParksList()
+		{
+			HttpResponseMessage response = await _httpClient.GetAsync("https://seriouslyfundata.azurewebsites.net/api/parks");
+			response.EnsureSuccessStatusCode();
+			return JsonConvert.DeserializeObject<List<Park>>(await response.Content.ReadAsStringAsync());
 		}
 	}
 }
--- Parks/Models/HelperFunctions.cs
+++ Parks/Models/HelperFunctions.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,6 @@ namespace Parks.Models
 {
 	public class HelperFunctions
 	{
-		private static readonly HttpClient _httpClient = new();
 		private readonly CacheService _cacheService;
 
 		public HelperFunctions(CacheService cacheService)
@@ -19,45 +19,8 @@ namespace Parks.Models
 
 		public async Task<ParkViewModel> GetParksData(string searchTerm)
 		{
-			bool cacheExists = _cacheService.CacheExists();
-
-			if (cacheExists)
-			{
-				List<Park> parkList = _cacheService.GetCachedData();
-				return CreateParkViewModel(parkList, searchTerm);
-			}
-			else
-			{
-				List<Park> parkList = await BuildParksList();
-				_cacheService.SetCachedData(parkList);
-				return CreateParkViewModel(parkList, searchTerm);
-			}
-		}
-
-		public async Task<List<Park>> BuildParksList()
-		{
-			HttpResponseMessage response = await _httpClient.GetAsync("https://seriouslyfundata.azurewebsites.net/api/parks");
-			response.EnsureSuccessStatusCode();
-			string responseContent = await response.Content.ReadAsStringAsync();
-
-			JArray parksResponse = JArray.Parse(responseContent);
-			List<Park> parkList = new List<Park>();
-
-			foreach (var park in parksResponse)
-			{
-				Park newParkRecord = new Park();
-				newParkRecord.ParkId = park["ParkID"].ToString();
-				newParkRecord.ParkName = park["Parkname"].ToString();
-				newParkRecord.SanctuaryName = park["SanctuaryName"].ToString();
-				newParkRecord.Borough = park["Borough"].ToString();
-				newParkRecord.Acres = Double.Parse(park["Acres"].ToString());
-				newParkRecord.Directions = park["Directions"].ToString();
-				newParkRecord.Description = park["Description"].ToString();
-				newParkRecord.HabitatType = park["HabitatType"].ToString();
-
-				parkList.Add(newParkRecord);
-			}
-			return parkList;
+			List<Park> parkList = await _cacheService.GetSetParksCache();
+			return CreateParkViewModel(parkList, searchTerm);
 		}
 
 		public ParkViewModel CreateParkViewModel(List<Park> parkList, string searchTerm)
